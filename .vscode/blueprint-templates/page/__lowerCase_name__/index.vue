1f34ca820dded336aa4a5015f36990454a7758e6
jennie.shao <m65pjj@gmail.com>
2021-10-29 04:23:46 +0800

    feat: add blueprint template

---
+++ .vscode/blueprint-templates/page/__lowerCase_name__/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <main></main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  setup() {}
+});
+</script>
+
+<style lang="scss" scoped></style>
