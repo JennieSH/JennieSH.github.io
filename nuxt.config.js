fb12302235bf2ef826d365d9b6ca07bfc2cf0a47
jennie.shao <m65pjj@gmail.com>
2021-11-01 06:16:33 +0800

    chore: add markdownit and highlight package

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["@/assets/main.scss"],
+  css: ["@/assets/main.scss", "highlight.js/styles/github-dark.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -38,12 +38,18 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     "@nuxtjs/axios",
-    "@nuxtjs/svg-sprite"
+    "@nuxtjs/svg-sprite",
+    "@nuxtjs/markdownit"
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {}
+  build: {},
+
+  markdownit: {
+    injected: true,
+    use: ["markdown-it-highlightjs"]
+  }
 };
