e696beac48e6eec6b68d6156ee3a96cd5903d3b0
jennie.shao <m65pjj@gmail.com>
2021-11-12 16:56:00 +0800

    feat: add ArticleList component

---
+++ components/ArticleItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <li>
+    <NuxtLink
+      :to="{
+        name: 'category-subject-article',
+        params: { subject: subject, article: article.fileName }
+      }"
+      :title="article.title"
+    >
+      <h2 class="font-bold text-lg">{{ article.title }}</h2>
+      <span class="text-sm text-gray-400">{{ article.publishedDate }}</span>
+      <p class="my-4 text-gray-800 dark:text-white">
+        {{ article.description }}
+      </p>
+      <Tag
+        v-for="(tag, index) in article.tags"
+        :key="`${index}.${tag}`"
+        :tag-name="tag"
+      />
+    </NuxtLink>
+  </li>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "@nuxtjs/composition-api";
+import { BasicInfo } from "@/types/content";
+
+export default defineComponent({
+  name: "ArticleItem",
+  props: {
+    subject: {
+      type: String,
+      default: ""
+    },
+    article: {
+      type: Object as PropType<BasicInfo>,
+      default: null
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+a {
+  @apply block p-4 shadow hover:shadow-lg rounded bg-light-block dark:bg-dark-block dark:hover:bg-opacity-60;
+
+  transition: all 0.3s ease-out;
+}
+</style>
---
+++ components/ArticleList.vue
@@ -0,0 +1,42 @@
+<template>
+  <section>
+    <h1 class="font-bold text-2xl capitalize">
+      {{ subject }}
+    </h1>
+    <ul v-if="articleMatterList.length > 0">
+      <ArticleItem
+        v-for="(article, index) in articleMatterList"
+        :key="`${index}.${article.title}`"
+        class="my-7"
+        :subject="subject"
+        :article="article"
+      />
+    </ul>
+    <p v-else class="mt-8">Sorry... It's empty now. 🙇‍♀️</p>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "@nuxtjs/composition-api";
+import { BasicInfo } from "@/types/content";
+
+export default defineComponent({
+  name: "ArticleList",
+  props: {
+    subject: {
+      type: String,
+      default: ""
+    },
+    articleMatterList: {
+      type: Array as PropType<BasicInfo[]>,
+      default: () => []
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+section {
+  @apply px-8 py-4 bg-light-bg dark:bg-dark-bg;
+}
+</style>
