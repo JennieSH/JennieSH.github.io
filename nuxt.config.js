4011d117c4a87414f8c26088b02de95459e538a5
jennie.shao <m65pjj@gmail.com>
2021-11-13 07:15:23 +0800

    feat: add meta tag config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,15 +1,50 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "Jennie's Note",
+    title: "傑尼海馬迴｜Jennie DEV",
     htmlAttrs: {
-      lang: "en"
+      lang: "zh-tw"
     },
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" }
+      { name: "format-detection", content: "telephone=no" },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "90% 開發筆記 + 10% 生活雜記，目前為前端工程師，紀錄開發時遇到的疑難雜症和學習筆記，偶爾穿插生活記事，透過文字延長記憶存放的期限。"
+      },
+      {
+        hid: "og:title",
+        property: "og:title",
+        content: "傑尼海馬迴｜Jennie DEV"
+      },
+      {
+        hid: "og:description",
+        property: "og:description",
+        content:
+          "90% 開發筆記 + 10% 生活雜記，目前為前端工程師，紀錄開發時遇到的疑難雜症和學習筆記，偶爾穿插生活記事，透過文字延長記憶存放的期限。"
+      },
+      {
+        hid: "og:url",
+        property: "og:url",
+        content: "https://JennieSH.github.io"
+      },
+      {
+        hid: "keywords",
+        name: "keywords",
+        content: "前端開發、技術部落格、旅遊"
+      },
+      {
+        hid: "og:site_name",
+        property: "og:site_name",
+        content: "傑尼海馬迴｜Jennie DEV"
+      },
+      { hid: "og:type", property: "og:type", content: "article" },
+      { hid: "og:image", property: "og:image", content: "/favicon.ico" },
+      { hid: "og:image:width", property: "og:image:width", content: "300" },
+      { hid: "og:image:height", property: "og:image:height", content: "300" }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
