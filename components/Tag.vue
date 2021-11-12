8f262422b054bfaad5ccfa5d5addc6edcbaf0a95
jennie.shao <m65pjj@gmail.com>
2021-11-12 16:31:41 +0800

    feat: add Tag component

---
+++ components/Tag.vue
@@ -0,0 +1,23 @@
+<template>
+  <span class="tag">{{ tagName }}</span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  name: "Tag",
+  props: {
+    tagName: {
+      type: String,
+      default: ""
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.tag {
+  @apply px-2 py-1 mx-1 text-xs tracking-wide rounded-full bg-gray-100 text-gray-500 dark:text-gray-50 dark:bg-gray-700;
+}
+</style>
