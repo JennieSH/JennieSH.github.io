77eee78ee2eeeb42ec70246f0707b09037f629cd
jennie.shao <m65pjj@gmail.com>
2022-02-12 04:58:03 +0800

    chore: change avatar link

--- types/link.ts
+++ types/link.ts
@@ -1,6 +1,7 @@
 enum Link {
   GITHUB = "https://github.com/JennieSH",
-  AVATAR = "https://images.unsplash.com/photo-1571988840298-3b5301d5109b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80"
+  // eslint-disable-next-line max-len
+  AVATAR = "https://scontent-tpe1-1.xx.fbcdn.net/v/t31.18172-8/22384136_1473536259409213_3155450266099013512_o.jpg?_nc_cat=109&ccb=1-5&_nc_sid=730e14&_nc_ohc=g7ZT3HiwBE8AX_0CLD5&_nc_ht=scontent-tpe1-1.xx&oh=00_AT-B7ed2gfiT9o7hzTDTPA0yWlv5Eolx2YW_qxAQ4vpsOw&oe=622C2BD5"
 }
 
 export default Link;
